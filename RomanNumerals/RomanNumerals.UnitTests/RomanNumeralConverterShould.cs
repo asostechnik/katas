29f6748df90b017123375c9aa14f521bbf2b2633
Nik Crabtree <nikc@asos.com>
2016-03-23 15:03:07 +0000

    Logic up to 50

---
+++ RomanNumerals/RomanNumerals.UnitTests/RomanNumeralConverterShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RomanNumerals.UnitTests
+{
+    [TestFixture]
+    public class RomanNumeralConverterShould
+    {
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(9, "IX")]
+        [TestCase(11, "XI")]
+        [TestCase(14, "XIV")]
+        [TestCase(39, "XXXIX")]
+        [TestCase(40, "XL")]
+        public void Convert_Arabic_Number_To_Roman_Numeral(int arabicNumber, string expectedRomanNumeral)
+        {
+            var romanNumeral = RomanNumeralConverter.Convert(arabicNumber);
+
+            Assert.That(romanNumeral, Is.EqualTo(expectedRomanNumeral));
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        private static readonly Dictionary<int, string> RomanNumerals = new Dictionary<int, string>()
+        {
+            { 1, "I" }, {4, "IV"}, { 5, "V" }, {9, "IX"}, { 10, "X" }, { 40, "XL" }
+        };
+
+        public static string Convert(int arabicNumber)
+        {
+            if (RomanNumerals.ContainsKey(arabicNumber))
+                return RomanNumerals[arabicNumber];
+
+            if (arabicNumber > 50)
+            {
+                return "L" + Convert(arabicNumber - 50);
+            }
+
+            if (arabicNumber > 10)
+            {
+                return "X" + Convert(arabicNumber - 10);
+            }
+
+            if (arabicNumber > 5)
+            {
+                return "V" + Convert(arabicNumber - 5);
+            }
+
+            return new string('I', arabicNumber);
+        }
+    }
+}
