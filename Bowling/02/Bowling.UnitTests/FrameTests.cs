7dafe3a36c51ada3e323c3f4c8d152e8eff9c368
Nik Crabtree <nikc@asos.com>
2016-03-24 09:59:59 +0000

    Added Frame

---
+++ Bowling/02/Bowling.UnitTests/FrameTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Bowling.UnitTests
+{
+    public class FrameTests
+    {
+        [TestFixture]
+        public class FrameShould
+        {
+            [Test]
+            public void Store_two_roll_scores()
+            {
+                //arrange
+                var frame = new Frame();
+
+                //act
+                frame.FirstRollScore = 1;
+                frame.SecondRollScore = 2;
+
+                //assert
+                frame.FirstRollScore.Should().Be(1);
+                frame.SecondRollScore.Should().Be(2);
+            }
+
+            [Test]
+            public void Add_roll_scores_together_to_give_frame_score()
+            {
+                //arrange
+                var frame = new Frame() { FirstRollScore = 1, SecondRollScore = 2 };
+
+                //act
+                var frameScore = frame.Score;
+
+                //assert
+                frameScore.Should().Be(3);
+            }
+        }
+    }
+}
---
+++ Bowling/02/Bowling/Frame.cs
@@ -0,0 +1,11 @@
+﻿namespace Bowling
+{
+    public class Frame
+    {
+        public int FirstRollScore { get; set; }
+
+        public int SecondRollScore { get; set; }
+
+        public int Score => FirstRollScore + SecondRollScore;
+    }
+}
